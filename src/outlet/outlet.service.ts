b60ebb98f944a3bb4bd706bbc82d58c3a1b14b8b
stevenwr92 <stevenwr92@gmail.com>
2023-07-05 13:34:31 +0700

    crud outlet

--- src/outlet/outlet.service.ts
+++ src/outlet/outlet.service.ts
@@ -1,10 +1,11 @@
-import { Injectable } from '@nestjs/common';
+import { ForbiddenException, Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
+import { OutletDto } from './dto';
 
 @Injectable()
 export class OutletService {
   constructor(private prisma: PrismaService) {}
-  async createOutlet(dto) {
+  async createOutlet(dto: OutletDto) {
     try {
       let outlet = await this.prisma.outlet.create({
         data: dto,
@@ -29,6 +30,49 @@ export class OutletService {
   async getOutletById(id: number) {
     try {
       return this.prisma.outlet.findFirst({
+        where: {
+          id,
+        },
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  async editOutletById(dto: OutletDto, id: number) {
+    try {
+      const outlet = await this.prisma.outlet.findUnique({
+        where: {
+          id,
+        },
+      });
+
+      if (!outlet) throw new ForbiddenException('Data not found');
+
+      return this.prisma.outlet.update({
+        where: {
+          id,
+        },
+        data: {
+          ...dto,
+        },
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  async deleteOutletById(id: number) {
+    try {
+      const outlet = await this.prisma.outlet.findUnique({
+        where: {
+          id,
+        },
+      });
+
+      if (!outlet) throw new ForbiddenException('Data not found');
+
+      await this.prisma.outlet.delete({
         where: {
           id,
         },
